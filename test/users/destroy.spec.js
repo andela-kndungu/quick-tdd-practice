05f33e1e0bae762287ae8959741bf389a02440b0
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-09-18 17:46:09 +0300

    Add `destroy user` tests

---
+++ test/users/destroy.spec.js
@@ -0,0 +1,53 @@
+import should from 'should'; // eslint-disable-line
+import request from 'supertest';
+import Users from '../../server/models/users.js';
+
+import app from '../../server/index.js';
+
+describe('Destroy', () => {
+  let user;
+  before((done) => {
+    Users.remove({}, () => {
+      const newUser = new Users();
+      newUser.username = 'normal';
+      newUser.name.first = 'Normal';
+      newUser.name.last = 'User';
+      newUser.email = 'normal@user.com';
+      newUser.password = 'normal';
+      newUser.save((error, createdUser) => {
+        if (error) {
+          throw error;
+        }
+        user = createdUser;
+        done();
+      });
+    });
+  });
+
+  describe('Successfully deletes a user', () => {
+    it('Deletes a user by id', () => {
+      request(app)
+        .delete(`/api/users?id=${user._id}`)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((error, res) => {
+          should.not.exist(error);
+          res.body.username.should.equal('normal');
+        });
+    });
+  });
+
+  describe('Handles non existent users', () => {
+    it('Returns error message on non existent user', () => {
+      request(app)
+        .get('/api/users?id=573b7edafe90559c354b81fd')
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .end((error, res) => {
+          should.not.exist(error);
+          res.body.message.should.equal('User does not exist');
+        });
+    });
+  });
+});
+
